387f496120d2eed60f73698513a1bb1f042bc8da
AllanYSalazarG <salazargarciaallanyudiel@hotmail.com>
2025-02-12 13:28:00 -0800

    Introducción a las clases en python

---
+++ 05 - clases/01-introduccion.py
@@ -0,0 +1,11 @@
+mensaje = "hola mundo"
+print(type(mensaje))
+
+# Clase: es el plano de construcción
+# Objeto: una instancia de una clase
+
+# Clase: plano de construccion de una casa
+# Objeto: la casa construida
+
+# Clase: Humano
+# Objeto: Allan, Diana, etc
