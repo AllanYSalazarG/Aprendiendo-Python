8a4ea17780afcf59946cdd114599a3fe5aec231e
AllanYSalazarG <salazargarciaallanyudiel@hotmail.com>
2025-02-13 09:34:28 -0800

    Agregando decoradores @property a getter y setter

---
+++ 05 - clases/07-decorador-properties.py
@@ -0,0 +1,17 @@
+class Dog:
+    def __init__(self, name,):
+        self.name = name
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        if name.strip():
+            self.__name = name
+        return
+
+
+dog = Dog("Choclo")
+print(dog.name)
