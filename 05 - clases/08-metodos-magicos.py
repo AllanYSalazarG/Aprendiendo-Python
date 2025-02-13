e5101637b35e330faf5a90522872df0e8e32eae8
AllanYSalazarG <salazargarciaallanyudiel@hotmail.com>
2025-02-13 09:44:26 -0800

    Agregando un destructor a la clase

--- 05 - clases/08-metodos-magicos.py
+++ 05 - clases/08-metodos-magicos.py
@@ -1,16 +1,21 @@
 class Dog:
+    # Constructor
     def __init__(self, name, age):
         self.name = name
         self.age = age
 
+    # Destructor
+    def __del__(self):
+        print(f"Chao {self.name}, te recordaremos")
+
+    # Método mágico
     def __str__(self):
         return f"Clase Perro: {self.name}"
 
+    # Método definido
     def ladra(self):
         print(f"{self.name}Guau!")
 
 
 dog = Dog("Chanchito", 7)
-print(dog)
-text = str(dog)
-print(text)
+del dog
